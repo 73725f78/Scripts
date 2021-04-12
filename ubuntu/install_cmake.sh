bed75bf180f16114ca7af603fb7c76db7b0665bf
73725f78 <harck0007@hotmail.com>
2021-04-12 14:29:32 -0500

    Install CMake

---
+++ ubuntu/install_cmake.sh
@@ -0,0 +1,8 @@
+sudo apt install build-essential libssl-dev
+
+wget https://github.com/Kitware/CMake/releases/download/v3.20.0/cmake-3.20.0.tar.gz
+tar -zxvf cmake-3.20.0.tar.gz
+cd cmake-3.20.0
+./bootstrap
+make 
+sudo make install 
