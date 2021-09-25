384172b64e5f6abd5d579e690dadf1fd17992626
luis <luisr@ontera.bio>
2021-09-25 02:58:29 -0500

    adding video icons

--- ubuntu/ubuntun_config.sh
+++ ubuntu/ubuntun_config.sh
@@ -9,4 +9,4 @@ sudo apt install gnome-session -y
 sudo apt install gnome-tweaks gnome-tweak-tool -y
 sudo apt install gnome-shell-extensions -y
 sudo apt install adwaita-icon-theme-full -y
-
+sudo apt install ffmpegthumbnailer -y
