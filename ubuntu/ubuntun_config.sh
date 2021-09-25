c481400d0ba6a7a1ffef3022ca3fdecc5e03fe9b
luis <luisr@ontera.bio>
2021-09-25 02:36:34 -0500

    Adding lost icons in ubuntu

--- ubuntu/ubuntun_config.sh
+++ ubuntu/ubuntun_config.sh
@@ -8,3 +8,5 @@ sudo apt install tmux -y
 sudo apt install gnome-session -y
 sudo apt install gnome-tweaks gnome-tweak-tool -y
 sudo apt install gnome-shell-extensions -y
+sudo apt install adwaita-icon-theme-full -y
+
