97e6cf79fc44d290c499e73a638f42739756b309
luis <harck0007@hotmail.com>
2022-08-11 02:53:35 -0500

    Update java version, add gradle

--- ubuntu_20/ubuntun_config.sh
+++ ubuntu_20/ubuntun_config.sh
@@ -18,38 +18,39 @@ sudo apt autoremove -y
 
 #sudo snap install vim-editor                    # 8.2.788
 sudo snap install cmake --classic
+sudo snap install gradle --classic
 sudo snap install shutter
 
-sudo apt install -y chromium-browser
-sudo apt install -y gparted
-sudo apt install -y vlc
-sudo apt install -y okular
-sudo apt install -y simplescreenrecorder
-sudo apt install -y thunderbird
-sudo apt install -y git
-sudo apt install -y tmux
 sudo apt install -y gnome-session
 sudo apt install -y gnome-tweaks gnome-tweak-tool
 sudo apt install -y gnome-shell-extensions
 sudo apt install -y adwaita-icon-theme-full
-sudo apt install -y ffmpegthumbnailer
+sudo apt install -y git
+sudo apt install -y vlc
+sudo apt install -y tmux
 sudo apt install -y unrar
+sudo apt install -y okular
+sudo apt install -y gparted
+sudo apt install -y minicom
+sudo apt install -y thunderbird
+sudo apt install -y dos2unix
 sudo apt install -y openssh-server
+sudo apt install -y chromium-browser
+sudo apt install -y ffmpegthumbnailer
+sudo apt install -y simplescreenrecorder
 
 sudo apt install -y gnuplot-x11
-sudo apt install -y minicom
-sudo apt install -y openjdk-8-dbg
-sudo apt install -y openjdk-8-demo
-sudo apt install -y openjdk-8-doc
-sudo apt install -y openjdk-8-jdk
-sudo apt install -y openjdk-8-jre
+sudo apt install -y openjdk-17-dbg
+sudo apt install -y openjdk-17-demo
+sudo apt install -y openjdk-17-doc
+sudo apt install -y openjdk-17-jdk
+sudo apt install -y openjdk-17-jre
 sudo apt install -y ant
 sudo apt install -y ivy
 
 sudo apt install -y python3 python3-dev
 sudo apt install -y ruby ruby-dev
 sudo apt install -y tcl tcl-dev
-sudo apt install -y dos2unix
 
 
 #atl-tab shows only apps from the current workspace (checar)
