54993f7c6a1bd53d6e02500f9302f9f9d822b903
Luis <harck0007@hotmail.com>
2022-09-16 01:09:54 -0500

    adding dependencies for petalinux 2022

--- ubuntu_20/petalinux_dependencies.sh
+++ ubuntu_20/petalinux_dependencies.sh
@@ -9,3 +9,5 @@ sudo apt install -y zlib1g:i386
 sudo apt install -y gcc-multilib
 sudo apt install -y libncurses5-dev
 sudo apt install -y net-tools
+
+sudo apt-get install iproute2 gawk python3 python build-essential gcc git make net-tools libncurses5-dev tftpd zlib1g-dev libssl-dev flex bison libselinux1 gnupg wget git-core diffstat chrpath socat xterm autoconf libtool tar unzip texinfo zlib1g-dev gcc-multilib automake zlib1g:i386 screen pax gzip cpio python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev pylint3
