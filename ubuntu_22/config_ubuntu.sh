74b9f307435cf6b224dbf20fd8656c9d061dec7c
luis <lrodriguez@tec.mx>
2023-04-10 16:58:04 -0600

    fix broken links

--- ubuntu_22/config_ubuntu.sh
+++ ubuntu_22/config_ubuntu.sh
@@ -1,9 +1,8 @@
 set -v
 
-wget https://www.freebsdnews.com/wp-content/uploads/linux-circuitry-taringa-5865330.jpg.png
-mv linux-circuitry-taringa-5865330.jpg.png ~/Pictures/linux-circuitry.png
-
+wget -O ~/Pictures/linux-circuitry.png https://w.wallha.com/ws/13/RLGoOEqN.png
 gsettings set org.gnome.desktop.background picture-uri "file:////$HOME/Pictures/linux-circuitry.png"
+gsettings set org.gnome.desktop.background picture-uri-dark "file:////$HOME/Pictures/linux-circuitry.png"
 
 touch ~/Templates/txt_file.txt
 
@@ -47,10 +46,12 @@ sudo apt install -y chromium-browser
 sudo apt install -y ffmpegthumbnailer
 sudo apt install -y rabbitvcs-nautilus
 sudo apt install -y simplescreenrecorder
+sudo apt install -y libxml2-utils
+sudo apt install -y graphviz
 #sudo apt install -y vim-nox
 
 sudo apt install -y gnuplot-x11
-sudo apt install -y python3 python3-dev
+sudo apt install -y python3 python3-dev python3-pip
 sudo apt install -y ruby ruby-dev
 sudo apt install -y tcl tcl-dev
 sudo apt install -y clang
@@ -105,17 +106,17 @@ rm apache-ant-1.10.12-bin.tar.gz
 mv apache-ant-1.10.12 /opt/
 
 #install Ivy
-wget https://dlcdn.apache.org//ant/ivy/2.5.0/apache-ivy-2.5.0-bin.tar.gz
-tar -xzf apache-ivy-2.5.0-bin.tar.gz
-rm apache-ivy-2.5.0-bin.tar.gz
-mv apache-ivy-2.5.0 /opt/
+wget https://dlcdn.apache.org//ant/ivy/2.5.1/apache-ivy-2.5.1-bin.tar.gz
+tar -xzf apache-ivy-2.5.1-bin.tar.gz
+rm apache-ivy-2.5.1-bin.tar.gz
+mv apache-ivy-2.5.1 /opt/
 mkdir -p /home/luis/.ant/lib
-cp /opt/apache-ivy-2.5.0/ivy-2.5.0.jar $HOME/.ant/lib
+cp /opt/apache-ivy-2.5.1/ivy-2.5.1.jar $HOME/.ant/lib
 
 #install eclipse
-wget https://mirror.csclub.uwaterloo.ca/eclipse/technology/epp/downloads/release/2022-09/R/eclipse-jee-2022-09-R-linux-gtk-x86_64.tar.gz
-tar -xzf eclipse-jee-2022-09-R-linux-gtk-x86_64.tar.gz
-rm  eclipse-jee-2022-09-R-linux-gtk-x86_64.tar.gz
+wget https://eclipse.mirror.rafal.ca/technology/epp/downloads/release/2023-03/R/eclipse-jee-2023-03-R-linux-gtk-x86_64.tar.gz
+tar -xzf eclipse-jee-2023-03-R-linux-gtk-x86_64.tar.gz
+rm  eclipse-jee-2023-03-R-linux-gtk-x86_64.tar.gz
 mv  eclipse /opt/
 
 sudo desktop-file-install eclipse.desktop
