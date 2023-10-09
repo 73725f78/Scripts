6add3f26a824baa4efbacc818e4ea00af54ebd1c
L <harck0007@hotmail.com>
2023-10-09 17:36:42 -0600

    revisar comentarios

--- ubuntu_22/install_nerd_fonts.sh
+++ ubuntu_22/install_nerd_fonts.sh
@@ -2,3 +2,18 @@ git clone https://github.com/ryanoasis/nerd-fonts.git
 cd nerd-fonts
 ./install.sh
 
+
+
+##   https://docs.rockylinux.org/books/nvchad/nerd_fonts/
+#
+#
+#
+##   Download the package with:
+##   
+##   https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/SourceCodePro.zip
+##   
+##   Next, unzip the contents of the folder and copy the fonts to ~/.local/share/fonts/ with:
+##   
+##   mkdir ~/.local/share/fonts
+##   unzip SourceCodePro.zip -d ~/.local/share/fonts/
+##   fc-cache ~/.local/share/fonts
