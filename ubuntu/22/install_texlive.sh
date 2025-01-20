d5cfb02b3477585d180c578346374df12cbad413
L <harck0007@hotmail.com>
2025-01-20 00:17:34 -0600

    update texlive mirror

--- ubuntu/22/install_texlive.sh
+++ ubuntu/22/install_texlive.sh
@@ -1,6 +1,7 @@
 sudo mkdir -p /mnt/iso
 
-wget https://ctan.mirrors.hoobly.com/systems/texlive/Images/texlive2024.iso
+#wget https://ctan.mirrors.hoobly.com/systems/texlive/Images/texlive2024.iso
+wget https://linux-mirror.xicalcoatl.mx/ctan/systems/texlive/Images/texlive2024.iso
 
 sudo mount -o loop texlive2024.iso /mnt/iso
 sudo /mnt/iso/install-tl --no-interaction
