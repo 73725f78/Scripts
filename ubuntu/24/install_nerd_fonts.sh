6c1759c4a1589d507f9a602905358e8a69abbc05
L <harck0007@hotmail.com>
2025-02-03 13:16:56 -0600

    update install nerdfonts to install only selected fonts.

--- ubuntu/24/install_nerd_fonts.sh
+++ ubuntu/24/install_nerd_fonts.sh
@@ -3,17 +3,14 @@ cd nerd-fonts
 ./install.sh
 
 
-
-##   https://docs.rockylinux.org/books/nvchad/nerd_fonts/
-#
-#
-#
-##   Download the package with:
-##   
-##   https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/SourceCodePro.zip
-##   
-##   Next, unzip the contents of the folder and copy the fonts to ~/.local/share/fonts/ with:
-##   
-##   mkdir ~/.local/share/fonts
-##   unzip SourceCodePro.zip -d ~/.local/share/fonts/
-##   fc-cache ~/.local/share/fonts
+## Download nerdfonts (select onle the required)
+## 	https://www.nerdfonts.com/font-downloads
+## 
+## Extract to:
+## 	~/.fonts/
+## 
+## run: 
+## 	$ fc-cache -fv
+## 
+## gvim:
+## 	select a nerd fonts to show icons (devicons plugin)
