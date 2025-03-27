406172ece5faa466cf838a68802d2b70f2c0f468
L <harck0007@hotmail.com>
2025-03-27 15:32:23 -0600

    config ubuntu update

--- ubuntu/24/bk/install_texlive.sh
+++ ubuntu/24/bk/install_texlive.sh
@@ -1,12 +1,12 @@
 sudo mkdir -p /mnt/iso
 
 #wget https://ctan.mirrors.hoobly.com/systems/texlive/Images/texlive2024.iso
-wget https://linux-mirror.xicalcoatl.mx/ctan/systems/texlive/Images/texlive2024.iso
+wget https://linux-mirror.xicalcoatl.mx/ctan/systems/texlive/Images/texlive2025.iso
 
-sudo mount -o loop texlive2024.iso /mnt/iso
+sudo mount -o loop texlive2025.iso /mnt/iso
 sudo /mnt/iso/install-tl --no-interaction
 sudo umount /mnt/iso
-rm texlive2024.iso
+rm texlive2025.iso
 
 # add to:  ~/.latexmkrc 
 # $pdf_previewer = 'okular';
